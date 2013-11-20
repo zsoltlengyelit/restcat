117947cfb0bce5b71afd752490afbf80531f01d0
Zsolt Lengyel <zsolt.lengyel.it@gmail.com>
2013-11-20 20:34:57 +0100

    - headers setting feature added  - unused js libs have removed

--- js/app/controllers.js
+++ js/app/controllers.js
@@ -15,6 +15,9 @@ app.controller('QueryCtrl', [ '$element', '$scope', '$http', function($element, 
     $scope.server = 'http://index.hu';
     $scope.path = '/';
     $scope.method = 'GET';
+    $scope.headers = [{}];
+    
+    $scope.hasResult = false;
 
     /**
      * Sends the query.
@@ -24,17 +27,54 @@ app.controller('QueryCtrl', [ '$element', '$scope', '$http', function($element, 
         var request = new XMLHttpRequest({mozSystem : true});
         var url = $scope.server + $scope.path;
         request.open($scope.method, url, true);
+        
+        angular.forEach(this.headers, function(header){            
+             var name = header.name;
+             
+             if(name){
+                 var value = header.value || '';             
+                 request.setRequestHeader(name, value);
+             }
+         });
+        
+        
         request.onreadystatechange = function() {
             if(this.readyState != 4) return; // wait for full response
             
             $scope.statusCode = this.status;
             $scope.queryResult = this.responseText;
             
+            $scope.hasResult = true;
             $scope.$apply();
         };
     
         request.send();
           
+    };
+    
+    $scope.addHeader = function(){
+      
+       var hasEmptyHeader = false;
+       angular.forEach(this.headers, function(header){
+           if(!header.name) hasEmptyHeader = true;
+           
+           return false;
+       });
+       
+       if(!hasEmptyHeader){
+           this.headers.push({});
+       }        
+    };
+    
+    $scope.makeEmpty = function(index){
+      
+        this.headers[index] = {};
+    };
+    
+    $scope.removeHeader = function(index){
+        if(this.headers.length == 1) return;
+        
+        this.headers.splice(index, 1);
     };
 
     // View controls
